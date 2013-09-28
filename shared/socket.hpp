d169c015f8b91224523922b5a57a94a022b5fccf
Cameron Morris <cmorris@cs.uml.edu>
2013-09-28 14:02:20 -0400

    Created socket class. No functionality yet.

--- shared/socket.hpp
+++ shared/socket.hpp
@@ -1,13 +1,64 @@
 /*****************************************************************************/
 /* Programmer: Cameron Morris                                                */
 /*                                                                           */
-/* Program Assignment:                                                       */
+/* Program Assignment: HTTP Client/Server                                    */
 /*                                                                           */
-/* File:                                                                     */
+/* File: socket.hpp                                                          */
 /*                                                                           */
-/* Description:                                                              */
+/* Description: Contains the class for a tcp socket using unix api           */
 /*                                                                           */
 /*                                                                           */
 /* Last updated: September 28, 2013 1:00 PM                                  */
 /*****************************************************************************/
 
+#include <sys/types.h>
+#include <sys/socket.h>
+
+enum SocketStates {
+
+  NOTREADY,
+  CONNECTED,
+  LISTENING,
+
+};
+
+class Socket {
+
+public:
+
+  // Creates socket on any port
+  Socket();
+
+  // Creates socket on specified port
+  Socket(unsigned short port);
+  
+  // Connect to host on port
+  int Connect( char ip[16], unsigned short port );
+
+  // Listens for connections
+  int Listen( int backlog = 5);
+
+  // Accept any pending connection
+  int Accept( struct sockaddr *remoteAddress );
+  
+  // Send data over socket
+
+  int Send( const void *data, int size );
+
+  // Receive data over socket
+
+  int Receive( void *data, int size );
+
+  
+  // Closes socket and cleans up
+  ~Socket();
+
+private:
+
+  // State of the socket
+  int state;
+
+  // Descriptor of the socket
+  int socketFD;
+  
+};
