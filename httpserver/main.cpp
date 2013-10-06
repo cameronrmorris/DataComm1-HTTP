f49f6334099213eb450f94f033228c8ecac62fe2
Cameron Morris <cmorris@cs.uml.edu>
2013-10-05 21:35:13 -0400

    Some server stuff

--- httpserver/main.cpp
+++ httpserver/main.cpp
@@ -1,21 +1,64 @@
 /*****************************************************************************/
 /* Programmer: Cameron Morris                                                */
 /*                                                                           */
-/* Program Assignment: HTTP Client                                           */
+/* Program Assignment: HTTP Server                                           */
 /*                                                                           */
-/* File: client.cpp                                                          */
+/* File: server.cpp                                                          */
 /*                                                                           */
-/* Description: This program is a basic HTTP client that can perform GET and */
-/*              PUT requests to a HTTP server.                               */
+/* Description: This program is a basic HTTP server that can accept GET and  */
+/*              PUT requests from a HTTP client and respond.                 */
 /*                                                                           */
 /* Last updated: September 28, 2013 1:00 PM                                  */
 /*****************************************************************************/
 
-
+#include "../shared/socket.hpp"
+#include <iostream>
+#include <unistd.h>
+#include <stdlib.h>
 
 int main( int argc, char *argv[] ) {
 
-  
+  Socket server;
+  Socket client;
+  char buffer[1024];
+  struct sockaddr_storage remoteAddr;
+
+  if( argc < 2 ) {
+
+    std::cout << "Usage: http_server <port>" << std::endl;
+    return -1;
+
+  }
+
+  // Bind socket to specified socket
+  server.BindPort(argv[1]);
+
+  // Set socket to listen mode
+  server.Listen();
+
+  // Begin server loop for requests
+  while( 1 ) {
+
+    client = server.Accept( &remoteAddr );
+
+    switch( fork() ) {
+
+      //Child
+    case 0:
+      // Receive the client request
+      std::cout << "Received client request" << std::endl;
+      while ( client.Receive( (void *)buffer, sizeof(buffer) ) )
+	std::cout << "Answering client: " << buffer  << std::endl;
+      client.Send( (void *)buffer, sizeof(buffer));
+      client.Close();
+      exit(0);
+      break;
+    default:
+      
+      break;
+    }
+  }
+
 
   return 0;
 
